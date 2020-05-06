2f08367c9de2b8c68d130d1da5d4bf5d2c39d3dd
Vermasoumitra020 <sonisoumitra@gmail.com>
2020-05-06 23:13:14 +0530

    Add signup & signin form

--- script/script.js
+++ script/script.js
@@ -1,3 +1,47 @@
+function switchTab(active, inactive)
+{
+    var which = active.innerHTML;
+    var signinContainer = document.getElementsByClassName('login-container')[0];
+    var signupContainer = document.getElementsByClassName('signup-container')[0];
+
+    if(which == 'Sign In')
+    {
+        signinContainer.style.display = "none";
+        signupContainer.style.display = "block";
+    }
+    else
+    {
+        signinContainer.style.display = "block";
+        signupContainer.style.display = "none";
+    }
+
+    active.style.backgroundColor = "rgb(250, 251, 252)";
+    active.style.borderBottom = "1px solid rgb(237, 240, 243)";
+    inactive.style.backgroundColor = "white";
+    inactive.style.borderBottom = "0";
+}
+
+function loginSignupTab()
+{
+    var tabs = document.getElementsByClassName("login-slider")[0];
+    var signin = tabs.getElementsByClassName("login-link")[0];
+    var signup = tabs.getElementsByClassName("signup-link")[0];
+
+    signup.addEventListener('click', function() {switchTab(signin, signup);}, false);
+    signin.addEventListener('click', function() {switchTab(signup, signin);}, false);
+
+    var SignInSignUpModel = document.getElementById('cred-model');
+    var closeTab = document.getElementsByClassName('close')[0];
+    closeTab.addEventListener('click', function() {closeModel(SignInSignUpModel);}, false)
+}
+
+function signInScreen()
+{
+    var regModel = document.getElementById('cred-model');
+    regModel.style.display = "block";
+    signInScreen();
+}
+
 function searchImg()
 {
     var imgContainer, imgs, img;
@@ -37,19 +81,19 @@ function searchImg()
     }
 }
 
-function closeImg(fullScreenModel)
+function closeModel(model)
 {
-    fullScreenModel.style.display = "none";
+    model.style.display = "none";
 }
 
 function expandImg(e)
 {
     var imgPath = e.target.getAttribute("src");
-    var fullScreenModel = document.getElementsByClassName("model")[0];
+    var fullScreenModel = document.getElementById("img-model");
     var fullScreenImage = fullScreenModel.getElementsByClassName("full-img")[0];
 
     var close = fullScreenModel.getElementsByClassName("close")[0];
-    close.addEventListener("click", function() {closeImg(fullScreenModel);}, false);
+    close.addEventListener("click", function() {closeModel(fullScreenModel);}, false);
 
     fullScreenImage.setAttribute("src", imgPath);
     fullScreenModel.style.display = "block";
@@ -57,6 +101,7 @@ function expandImg(e)
 
 function start()
 {
+    loginSignupTab();
     var searchBtn = document.getElementsByClassName("search-btn")[0];
     searchBtn.addEventListener("click", searchImg, false);
 
@@ -65,6 +110,7 @@ function start()
     {
         imgContaner[i].addEventListener("click", expandImg, false);
     }
+    document.getElementsByClassName('nav-signup')[0].addEventListener('click', signInScreen, false);
 }
 
 window.addEventListener("load", start, false);
